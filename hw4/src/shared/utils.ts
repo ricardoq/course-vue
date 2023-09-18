e0d44a46dba2ec025038c8a7e4029135b9400b4e
Ricardo Quinonez <vir_qup_91@hotmail.com>
2023-09-17 22:04:33 -0600

    Fix sorts

--- hw4/src/shared/utils.ts
+++ hw4/src/shared/utils.ts
@@ -1,7 +1,8 @@
 export const snakeToCamel = (snakeCaseValue: string): string => {
   return snakeCaseValue.toLowerCase().replace(/([-_][a-z])/g, group =>
-  group
-    .replace('-', '')
-    .replace('_', '')
-);;
+    group
+      .toUpperCase()
+      .replace('-', '')
+      .replace('_', '')
+  );
 }
