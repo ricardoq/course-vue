c14da0ef101218c739adbc0c381d11d2f09b6e56
Ricardo Quinonez <vir_qup_91@hotmail.com>
2023-10-09 23:34:04 -0600

    add movie genre list

--- hw4/src/pages/MovieDetailPage/MovieDetailPage.vue
+++ hw4/src/pages/MovieDetailPage/MovieDetailPage.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="page-wrapper">
     <MovieDetail :movie="selectedMovie"></MovieDetail>
-    <MovieList></MovieList>
+    <MovieGenreList :genre="(selectedMovie as IMovie).genres[0]"></MovieGenreList>
   </div>
 </template>
 
 <script lang="ts" setup>
 import MovieDetail from '../../components/MovieDetailComponent/MovieDetailComponent.vue';
-import MovieList from '../../components/MovieListComponent/MovieListComponent.vue';
+import MovieGenreList from '../../components/MovieGenreListComponent/MovieGenreListComponent.vue';
 import useMoviesStore from '@/stores/useMoviesStore';
 import store from '@/stores/pinia';
+import type {IMovie} from '@/shared/definitions';
 
 const {selectedMovie} = useMoviesStore(store);
 </script>
